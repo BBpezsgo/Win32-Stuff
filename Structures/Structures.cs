750aa2eb55fdb11e949e91c0fb622d448421bc2e
BB pezsgő <bazsi5fel2@gmail.com>
2023-10-09 15:46:32 +0200

    Small improvements

--- Structures/Structures.cs
+++ Structures/Structures.cs
@@ -598,8 +598,46 @@ namespace Win32
         public short Right;
         public short Bottom;
 
-        public readonly short Width => (short)(Right - Left + 1);
-        public readonly short Height => (short)(Bottom - Top + 1);
+        public short X
+        {
+            readonly get => Left;
+            set => Left = value;
+        }
+
+        public short Y
+        {
+            readonly get => Top;
+            set => Top = value;
+        }
+
+        public short Width
+        {
+            readonly get => (short)(Right - Left + 1);
+            set => Right = (short)(Left + value);
+        }
+        public short Height
+        {
+            readonly get => (short)(Bottom - Top + 1);
+            set => Bottom = (short)(Top + value);
+        }
+
+        public static SmallRect Zero => default;
+
+        public System.Drawing.Point Location
+        {
+            get
+            {
+                return new System.Drawing.Point(Left, Top);
+            }
+        }
+
+        public static SmallRect FromPosAndSize(int x, int y, int width, int height) => new()
+        {
+            Left = (short)x,
+            Top = (short)y,
+            Bottom = (short)(y + height),
+            Right = (short)(x + width),
+        };
 
         public override readonly bool Equals(object? obj) =>
             obj is SmallRect rect
@@ -617,6 +655,18 @@ namespace Win32
 
         public override readonly string ToString()
             => $"{{ Left: {Left} Top: {Top} Bottom: {Bottom} Right: {Right} }}";
+
+        public readonly bool Contains(Point point) =>
+            point.X >= Left &&
+            point.Y >= Top &&
+            point.X < Right &&
+            point.Y < Height;
+
+        public readonly bool Contains(int x, int y) =>
+            x >= Left &&
+            y >= Top &&
+            x < Right &&
+            y < Height;
     }
 
     public struct InputMode
---
+++ Utilities/Console/PredefinedByteColors.cs
@@ -0,0 +1,24 @@
+﻿namespace Win32
+{
+    public struct ByteColor
+    {
+        public const byte Red = 0b_0100;
+        public const byte Green = 0b_0010;
+        public const byte Blue = 0b_0001;
+        public const byte Yellow = 0b_0110;
+        public const byte Cyan = 0b_0011;
+        public const byte Magenta = 0b_0101;
+
+        public const byte BrightRed = 0b_1100;
+        public const byte BrightGreen = 0b_1010;
+        public const byte BrightBlue = 0b_1001;
+        public const byte BrightYellow = 0b_1110;
+        public const byte BrightCyan = 0b_1011;
+        public const byte BrightMagenta = 0b_1101;
+
+        public const byte Black = 0b_0000;
+        public const byte Silver = 0b_0111;
+        public const byte Gray = 0b_1000;
+        public const byte White = 0b_1111;
+    }
+}
